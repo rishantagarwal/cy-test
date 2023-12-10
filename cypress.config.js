29d9ebcac522ac1b01cd92b67e189fc48a4555ef
Rishant Agarwal <bindasrishant@gmail.com>
2023-12-10 20:11:34 +0900

    chore: test cy cloud record config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'z66pr8',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
